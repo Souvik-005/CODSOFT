f4bbc8dabec002adde9ba9dcf826a730e6844d9e
Souvik-005 <souvikbanerjee9732@gmail.com>
2026-03-06 23:26:35 +0530

    Update Calculator logic

--- Calculator/app.py
+++ Calculator/app.py
@@ -8,30 +8,38 @@ while True:
         print("4. Division ")
         print("5. Remainder")
         print("6. Exit")
-        Op1=int(input("Enter 1st Number:: "))
-        Op2=int(input("Enter 2nd Number:: "))
         choice=int(input("Choose your Operation to perform: "))
         if choice == 1:
+            Op1=int(input("Enter 1st Number:: "))
+            Op2=int(input("Enter 2nd Number:: "))
             print(f"The Addition of {Op1} and {Op2} is: {Op1+Op2}")
         elif choice == 2:
-                print(f"The Subtraction of {Op1} and {Op2} is: {Op1-Op2}")
+            Op1=int(input("Enter 1st Number:: "))
+            Op2=int(input("Enter 2nd Number:: "))   
+            print(f"The Subtraction of {Op1} and {Op2} is: {Op1-Op2}")
         elif choice == 3:
+            Op1=int(input("Enter 1st Number:: "))
+            Op2=int(input("Enter 2nd Number:: "))
             print(f"The Multiplication of {Op1} and {Op2} is: {Op1*Op2}")
         elif choice == 4:
+            Op1=int(input("Enter 1st Number:: "))
+            Op2=int(input("Enter 2nd Number:: "))
             if Op2 == 0:
                 print("Can't Divide by Zero.")
             else:
                 print(f"The Division of {Op1} and {Op2} is: {Op1/Op2}")
                 print(f"The Integer Division of {Op1} and {Op2} is: {Op1//Op2}")
         elif choice == 5:
+            Op1=int(input("Enter 1st Number:: "))
+            Op2=int(input("Enter 2nd Number:: "))
             if Op2 == 0:
                 print("Can't Divide by Zero.")
             else:
                 print(f"The Remainder of {Op1} and {Op2} is: {Op1%Op2}")   
         elif choice == 6:
+            print("Thank you...")
             break
         else:
             print("Invalid Choice , please Select the right Choice.")
     except ValueError:
         print("Enter a Valid Input.")
-print("Thank you...")
